7f2e4c9aa41f4ab0a313a3b26e4328f43ec23910
”ziobro” <nikodem.ziobro@nokia.com>
2024-02-16 19:48:39 +0100

    Planter app

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const mainParagraph = document.getElementById("mainParagraph");
+const title = document.getElementById("title");
+const addPlantButton = document.getElementById("addPlant");
+mainParagraph.innerText = "No plants still :("
+
+var grubosz =
+    {
+        lastWatering: date,
+        daysFrom: daysFrom
+    }
+
+function addPlant() {
+    alert("asd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
